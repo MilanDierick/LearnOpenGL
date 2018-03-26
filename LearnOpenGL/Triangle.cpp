722e0891279db7c0ed90c44956326378b8d528fa
Milan Dierick <milan.dierick@gmail.com>
2018-03-26 14:39:15 +0200

    Working on triangle class.

---
+++ LearnOpenGL/Triangle.cpp
@@ -0,0 +1,50 @@
+﻿#include "Triangle.h"
+
+float* Triangle::getVertices() const
+{
+	return vertices;
+}
+
+void Triangle::setVertices(float* const vertices)
+{
+	this->vertices = vertices;
+}
+
+GLuint Triangle::getVao() const
+{
+	return vao;
+}
+
+void Triangle::setVao(const GLuint vao)
+{
+	this->vao = vao;
+}
+
+GLuint Triangle::getVbo() const
+{
+	return vbo;
+}
+
+void Triangle::setVbo(const GLuint vbo)
+{
+	this->vbo = vbo;
+}
+
+Triangle::Triangle(float* vertices, const GLuint vao, const GLuint vbo)
+{
+	this->vertices = vertices;
+	this->vao = vao;
+	this->vbo = vbo;
+
+	glGenBuffers(1, &this->vbo);
+	glBindVertexArray(this->vao);
+
+	glBindBuffer(GL_ARRAY_BUFFER, this->vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(this->vertices), this->vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), static_cast<void*>(nullptr));
+	glEnableVertexAttribArray(0);
+
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
---
+++ LearnOpenGL/Triangle.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <glad/glad.h>
+
+class Triangle
+{
+public:
+	float* getVertices() const;
+	void setVertices(float* const vertices);
+	GLuint getVao() const;
+	void setVao(const GLuint vao);
+	GLuint getVbo() const;
+	void setVbo(const GLuint vbo);
+	Triangle(float* vertices, GLuint vao, GLuint vbo);
+
+private:
+	float *vertices;
+	GLuint vao;
+	GLuint vbo;
+};
